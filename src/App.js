9b1ba20758985af0a38311b89186a0716016923f
Surya T N <suryatej.nim@yahoo.com>
2022-11-18 19:05:34 +0000

    Added required imports from third-party npm packages and bare-bones components with code for front-end react routing, that maps paths to components to be rendered in the UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import {Component} from 'react'
+import styled from 'styled-components'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import Popup from 'reactjs-popup'
+
 import './App.css'
 
-// Replace your code here
-const App = () => <div>Hello World</div>
+const Login = () => {}
+const Home = () => {}
+const Trending = () => {}
+const Gaming = () => {}
+const SavedVideos = () => {}
+const NotFound = () => {}
+const VideoItemDetails = () => {}
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/trending" component={Trending} />
+    <Route exact path="/gaming" component={Gaming} />
+    <Route exact path="/saved-videos" component={SavedVideos} />
+    <Route exact path="/videos/{id}" component={VideoItemDetails} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect path="/not-found" />
+  </Switch>
+)
 
 export default App
