58cf70b6c0d3cef00f6c647f28fe7366825ed999
Surya T N <suryatej.nim@yahoo.com>
2022-11-18 19:31:20 +0000

    Added more imports and turned App component to a class component, with required state object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import {Component} from 'react'
-import styled from 'styled-components'
 import {Switch, Route, Redirect} from 'react-router-dom'
+import {formatDistanceToNow} from 'date-fns'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
 import Popup from 'reactjs-popup'
+import styled from 'styled-components'
 
 import './App.css'
 
@@ -13,17 +16,29 @@ const SavedVideos = () => {}
 const NotFound = () => {}
 const VideoItemDetails = () => {}
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/trending" component={Trending} />
-    <Route exact path="/gaming" component={Gaming} />
-    <Route exact path="/saved-videos" component={SavedVideos} />
-    <Route exact path="/videos/{id}" component={VideoItemDetails} />
-    <Route exact path="/not-found" component={NotFound} />
-    <Redirect path="/not-found" />
-  </Switch>
-)
+class App extends Component {
+  state = {
+    videosList: [],
+    searchQuery: '',
+    isUserLoggedIn: false,
+  }
+
+  render() {
+    const {videosList, searchQuery, isUserLoggedIn} = this.state
+
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/trending" component={Trending} />
+        <Route exact path="/gaming" component={Gaming} />
+        <Route exact path="/saved-videos" component={SavedVideos} />
+        <Route exact path="/videos/{id}" component={VideoItemDetails} />
+        <Route exact path="/not-found" component={NotFound} />
+        <Redirect path="/not-found" />
+      </Switch>
+    )
+  }
+}
 
 export default App
