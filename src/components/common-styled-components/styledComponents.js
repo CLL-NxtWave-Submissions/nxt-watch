6dcb197e9fb7c24995fe0641dc0ccabd70d1df4e
Surya T N <suryatej.nim@yahoo.com>
2022-12-01 14:33:45 +0000

    Implemented styled components to render JSX elements of the common Header component, to be across multiple routes

--- src/components/common-styled-components/styledComponents.js
+++ src/components/common-styled-components/styledComponents.js
@@ -8,6 +8,42 @@ export const NxtWatchHeader = styled.div`
   align-items: center;
   margin: 0;
   padding: 1rem;
+  background-color: ${props => (props.isDarkTheme ? '#313131' : '#ffffff')};
 `
 
-// export const NxtWatchLogo
+export const NxtWatchLogo = styled.img`
+  height: 3rem;
+  width: 10rem;
+`
+
+export const HeaderActionsContainer = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+`
+
+export const HeaderActionItem = styled.li`
+  padding: 0 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const HeaderActionItemButton = styled.button`
+  color: ${props => (props.isDarkTheme ? '#f9f9f9' : '#181818')};
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: inherit;
+`
+
+export const UserProfileImg = styled.img`
+  height: 3rem;
+  width: 3rem;
+`
+
+export const LogoutBtn = styled(HeaderActionItemButton)`
+  color: ${props => (props.isDarkTheme ? '#f9f9f9' : '#4f46e5')};
+  border: 0.1rem solid ${props => (props.isDarkTheme ? '#f9f9f9' : '#4f46e5')};
+`
