b7bbc1bd62d26dee35de0470a9013f1c95f5bc63
Surya T N <suryatej.nim@yahoo.com>
2022-12-06 17:53:01 +0000

    Implemented styled components to render JSX elements in NavLinksMenu common component

---
+++ src/components/common-components/NavLinksMenu/styledComponents.js
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+export const NavLinksMenuContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-left: 0;
+  background-color: ${props => (props.isDarkTheme ? '#0f0f0f' : '#ffffff')};
+`
+
+export const NavLinksMenuItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props =>
+    props.isDarkTheme
+      ? `${props.isSelected ? '#313131' : 'inherit'}`
+      : `${props.isSelected ? '#ebebeb' : 'inherit'}`};
+`
+
+export const NavLink = styled(Link)`
+  text-decoration: none;
+`
+
+export const NavLinksMenuItemName = styled.label`
+  color: ${props =>
+    props.isDarkTheme
+      ? `${props.isSelected ? '#ffffff' : '#ebebeb'}`
+      : `${props.isSelected ? '#000000' : '#606060'}`};
+  font-family: 'Roboto';
+  font-size: 1.2rem;
+  font-weight: ${props => (props.isSelected ? '550' : '400')};
+`
