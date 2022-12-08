dd7dd2683fc0387fcd248f9229c446065c636051
Surya T N <suryatej.nim@yahoo.com>
2022-12-08 03:14:26 +0000

    Removed dataFetchRequestUrls object with mapping of routes to respective api request urls, from the common-module, as they have been added within the routed components for a modular maintainability

--- src/components/common-module/index.js
+++ src/components/common-module/index.js
@@ -3,16 +3,6 @@ import {HiFire} from 'react-icons/hi'
 import {SiYoutubegaming} from 'react-icons/si'
 import {BiListPlus} from 'react-icons/bi'
 
-// Urls for requesting data necessary to render
-// content UI for different routes.
-export const dataFetchRequestUrls = {
-  login: 'https://apis.ccbp.in/login',
-  home: 'https://apis.ccbp.in/videos/all?search=',
-  trending: 'https://apis.ccbp.in/videos/trending',
-  gaming: 'https://apis.ccbp.in/videos/gaming',
-  videoItemDetails: 'https://apis.ccbp.in/videos/',
-}
-
 // Possible state of a fetch API request
 export const apiRequestStates = {
   initial: 'INITIAL',
