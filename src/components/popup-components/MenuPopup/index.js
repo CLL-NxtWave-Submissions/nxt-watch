28aeed76f3e057392e095dd47221800c6d72b4e1
Surya T N <suryatej.nim@yahoo.com>
2022-12-08 07:35:20 +0000

    Implemented PopUI, MenuPopup components to render the popup when clicked on the mobile hamburger menu icon rendered in a JSX button element, in the Header component

---
+++ src/components/popup-components/MenuPopup/index.js
@@ -0,0 +1,53 @@
+import Popup from 'reactjs-popup'
+import {IconContext} from 'react-icons'
+import {MdClose} from 'react-icons/md'
+
+import NavLinksMenu from '../../common-components/NavLinksMenu'
+import {
+  MenuPopupBgContainer,
+  MenuPopupCloseContainer,
+  MenuPopupCloseButton,
+  MenuPopupContentContainer,
+} from './styledComponents'
+
+import AppContext from '../../context/AppContext'
+
+const PopupUI = props => {
+  const {popupCloseHandler} = props
+
+  return (
+    <AppContext.Consumer>
+      {appContextData => {
+        const {isDarkTheme} = appContextData
+
+        return (
+          <MenuPopupBgContainer isDarkTheme={isDarkTheme}>
+            <MenuPopupCloseContainer>
+              <MenuPopupCloseButton isDarkTheme={isDarkTheme}>
+                <IconContext.Provider value={{height: '2rem', width: '2rem'}}>
+                  <MdClose />
+                </IconContext.Provider>
+              </MenuPopupCloseButton>
+            </MenuPopupCloseContainer>
+
+            <MenuPopupContentContainer>
+              <NavLinksMenu popupCloseHandler={popupCloseHandler} />
+            </MenuPopupContentContainer>
+          </MenuPopupBgContainer>
+        )
+      }}
+    </AppContext.Consumer>
+  )
+}
+
+const MenuPopup = props => {
+  const {popupTrigger} = props
+
+  return (
+    <Popup modal trigger={popupTrigger}>
+      {close => <PopupUI popupCloseHandler={close} />}
+    </Popup>
+  )
+}
+
+export default MenuPopup
