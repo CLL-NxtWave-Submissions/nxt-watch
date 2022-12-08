9302174fbbc101c4722621c860d034f1fc97bb33
Surya T N <suryatej.nim@yahoo.com>
2022-12-08 09:19:24 +0000

    Replaced NavLinksMenuItemButton styled component with NavLink in NavLinksMenu component

--- src/components/common-components/NavLinksMenu/index.js
+++ src/components/common-components/NavLinksMenu/index.js
@@ -1,71 +1,64 @@
 import {
-  NavLinksMenuContainer,
-  NavLinkItem,
-  NavLinksMenuItemButton,
-  NavLinksMenuItemName,
+    NavLinksMenuContainer,
+    NavLinkItem,
+    NavLinksMenuItemButton,
+    NavLinksMenuItemName,
 } from './styledComponents'
 
-import {navLinksData} from '../../common-module'
+import { navLinksData } from '../../common-module'
 
 import AppContext from '../../context/AppContext'
 
 const NavLinksMenuItem = props => {
-  const {itemData, popupCloseHandler} = props
-  const {id, name, icon} = itemData
+    const { itemData, popupCloseHandler } = props
+    const { id, url, name, icon } = itemData
 
-  return (
-    <AppContext.Consumer>
-      {appContextData => {
-        const {isDarkTheme, selectedNavItemId, onNavItemSelect} = appContextData
-        const isItemSelected = id === selectedNavItemId
+    return (
+        <AppContext.Consumer>
+            {appContextData => {
+                const { isDarkTheme, selectedNavItemId, onNavItemSelect } = appContextData
+                const isItemSelected = id === selectedNavItemId
 
-        return (
-          <NavLinkItem isDarkTheme={isDarkTheme} isSelected={isItemSelected}>
-            <NavLinksMenuItemButton
-              type="button"
-              onClick={() =>
-                popupCloseHandler === undefined
-                  ? onNavItemSelect(id)
-                  : onNavItemSelect(id, popupCloseHandler)
-              }
-            >
-              {icon}
-              <NavLinksMenuItemName
-                isDarkTheme={isDarkTheme}
-                isSelected={isItemSelected}
-              >
-                {name}
-              </NavLinksMenuItemName>
-            </NavLinksMenuItemButton>
-          </NavLinkItem>
-        )
-      }}
-    </AppContext.Consumer>
-  )
+                return (
+                    <NavLinkItem isDarkTheme={isDarkTheme} isSelected={isItemSelected}>
+                        <NavLink to={url}>
+                            {icon}
+                            <NavLinksMenuItemName
+                                isDarkTheme={isDarkTheme}
+                                isSelected={isItemSelected}
+                            >
+                                {name}
+                            </NavLinksMenuItemName>
+                        </NavLink>
+                    </NavLinkItem>
+                )
+            }}
+        </AppContext.Consumer>
+    )
 }
 
 const NavLinksMenu = props => {
-  const {popupCloseHandler} = props
+    const { popupCloseHandler } = props
 
-  return (
-    <AppContext.Consumer>
-      {appContextData => {
-        const {isDarkTheme} = appContextData
+    return (
+        <AppContext.Consumer>
+            {appContextData => {
+                const { isDarkTheme } = appContextData
 
-        return (
-          <NavLinksMenuContainer isDarkTheme={isDarkTheme}>
-            {navLinksData.map(navLinkItemData => (
-              <NavLinksMenuItem
-                key={navLinkItemData.id}
-                itemData={navLinkItemData}
-                popupCloseHandler={popupCloseHandler}
-              />
-            ))}
-          </NavLinksMenuContainer>
-        )
-      }}
-    </AppContext.Consumer>
-  )
+                return (
+                    <NavLinksMenuContainer isDarkTheme={isDarkTheme}>
+                        {navLinksData.map(navLinkItemData => (
+                            <NavLinksMenuItem
+                                key={navLinkItemData.id}
+                                itemData={navLinkItemData}
+                                popupCloseHandler={popupCloseHandler}
+                            />
+                        ))}
+                    </NavLinksMenuContainer>
+                )
+            }}
+        </AppContext.Consumer>
+    )
 }
 
 export default NavLinksMenu
